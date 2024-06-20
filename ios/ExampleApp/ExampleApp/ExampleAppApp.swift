158f572b670b67875b17fdf1a3c7d07937ca270d
Jason.Huang <jason.huang@okg.com>
2024-06-20 17:38:55 +0800

    init

---
+++ ios/ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,22 @@
+
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by User Name on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // Enable the view you want
+            MultiplierCircuitView()
+            //KeccakCircuitView()
+            //KeccakDylibCircuitView()
+            //Halo2CircuitView()
+        }
+    }
+}
